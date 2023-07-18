ff2f2eb0a805ab0f8830d81a3d9ec867fbea77fe
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-07-18 15:01:28 -0300

    feat: created theme with colors used in the app and added the theme to the app

--- App.tsx
+++ App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 import {SafeAreaView} from 'react-native';
 import {Text} from './src/components/Text/Text';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text bold preset="headingLarge" style={{color: 'blue'}}>
-        Coffstack
-      </Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text bold preset="headingLarge" style={{color: 'blue'}}>
+          Coffstack
+        </Text>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/theme.ts
@@ -0,0 +1,40 @@
+/* eslint-disable prettier/prettier */
+import {createTheme} from '@shopify/restyle';
+
+const palette = {
+  greenPrimary: '#074C4E',
+  greenPrimaryLight: '#EAF6F6',
+  carrotSecondary: '#F86F2D',
+  carrotSecondaryLight: '#FAE6DD',
+  greenSuccess: '#4ABC86',
+  greenSuccessLight: '#D8FFEC',
+  redError: '#EA3838',
+  redErrorLight: '#FBECEC',
+
+  grayBlack: '#000000',
+  gray1: '#636363',
+  gray2: '#8E8E8E',
+  gray3: '#B3B3B3',
+  gray4: '#E1E1E1',
+  gray5: '#F5F5F5',
+  grayWhite: '#FFFFFF',
+};
+
+export const theme = createTheme({
+  colors: {
+    ...palette,
+    primary: palette.greenPrimary,
+    primaryContrast: palette.grayWhite,
+    background: palette.grayWhite,
+    backgroundContrast: palette.grayBlack,
+    error: palette.redError,
+    errorLight: palette.redErrorLight,
+    success: palette.greenSuccess,
+    sucessLight: palette.greenSuccessLight,
+    buttonPrimary: palette.greenPrimary,
+  },
+  spacing: {},
+});
+
+// extraindo o type diretamente do objeto
+export type Theme = typeof theme;
