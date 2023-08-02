605b18dd5d68b0e1141e9704859eab41a0339084
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-08-02 14:15:23 -0300

    feat(login page): created initial structure of the login page

--- App.tsx
+++ App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
 
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, TextInput, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 
-import {EyeOffIcon} from './src/assets/icons/EyeOffIcon';
-import {EyeOnIcon} from './src/assets/icons/EyeOnIcon';
-import {Icon} from './src/components/Icon/Icon';
+import {Box} from './src/components/Box/Box';
+import {Button} from './src/components/Button/Button';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <Text bold preset="headingLarge">
-          Coffstack
-        </Text>
-        <EyeOffIcon />
-        <EyeOnIcon />
-        <Icon name="eyeOff" color="error" size={90} />
+        <View style={{paddingHorizontal: 24}}>
+          <Text marginBottom="s8" preset="headingLarge">
+            Olá!
+          </Text>
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+          <Box mb="s20">
+            <TextInput
+              placeholder="Digite seu e-mail"
+              style={{borderWidth: 1, height: 50}}
+            />
+          </Box>
+          <Box>
+            <TextInput
+              placeholder="Digite sua senha"
+              style={{borderWidth: 1, height: 50}}
+            />
+          </Box>
+          <Text mt="s10" color="primary" preset="paragraphSmall" bold>
+            Esqueci minha senha
+          </Text>
+          <Button mt="s48" title="Entrar" />
+          <Button preset="outline" mt="s12" title="Criar uma conta" />
+        </View>
       </SafeAreaView>
     </ThemeProvider>
   );
