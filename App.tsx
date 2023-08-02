2422fca85557139c507476030ad7ebeeaeb198dd
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-08-02 15:58:30 -0300

    feat(input component): created TextInput component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import {SafeAreaView, TextInput, View} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 
 import {Box} from './src/components/Box/Box';
 import {Button} from './src/components/Button/Button';
+import {TextInput} from './src/components/TextInput/TextInput';
+import {Icon} from './src/components/Icon/Icon';
 
 function App(): JSX.Element {
   return (
@@ -19,19 +21,21 @@ function App(): JSX.Element {
           <Text preset="paragraphLarge" mb="s40">
             Digite seu e-mail e senha para entrar
           </Text>
-          <Box mb="s20">
-            <TextInput
-              placeholder="Digite seu e-mail"
-              style={{borderWidth: 1, height: 50}}
-            />
-          </Box>
-          <Box>
-            <TextInput
-              placeholder="Digite sua senha"
-              style={{borderWidth: 1, height: 50}}
-            />
-          </Box>
-          <Text mt="s10" color="primary" preset="paragraphSmall" bold>
+
+          <TextInput
+            label="E-mail"
+            placeholder="Digite seu e-mail"
+            boxProps={{mb: 's10'}}
+          />
+
+          <TextInput
+            label="Senha"
+            placeholder="Digite sua senha"
+            boxProps={{mb: 's10'}}
+            RightComponent={<Icon color="gray2" name="eyeOn" />}
+          />
+
+          <Text color="primary" preset="paragraphSmall" bold>
             Esqueci minha senha
           </Text>
           <Button mt="s48" title="Entrar" />
