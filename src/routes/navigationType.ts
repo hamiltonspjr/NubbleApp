145760e322dfa473f1270a6ee0c7b1a09c9a449b
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-08-25 19:33:57 -0300

    feat(tabNavigation): created the typing for navigating the app screens with tab navigation

--- src/routes/navigationType.ts
+++ src/routes/navigationType.ts
@@ -1,6 +1,9 @@
+import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
+import {CompositeScreenProps} from '@react-navigation/native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 import {AppStackParamList} from './AppStack';
+import {AppTabBottomTabParamList} from './AppTabNavigator';
 import {AuthStackParamList} from './AuthStack';
 
 declare global {
@@ -15,3 +18,12 @@ export type AppScreenProps<RouteName extends keyof AppStackParamList> =
 
 export type AuthScreenProps<RouteName extends keyof AuthStackParamList> =
   NativeStackScreenProps<AuthStackParamList, RouteName>;
+
+// interface genérica para navegação de tabs
+
+export type AppTabScreenProps<
+  RouteName extends keyof AppTabBottomTabParamList,
+> = CompositeScreenProps<
+  BottomTabScreenProps<AppTabBottomTabParamList, RouteName>,
+  NativeStackScreenProps<AppStackParamList, 'AppTabNavigator'>
+>;
