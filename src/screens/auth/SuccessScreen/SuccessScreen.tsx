1f7db42940415f18a316d542c6259bfad75430b3
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-08-25 19:09:16 -0300

    feat(tabNavigator): added libray for tab navigator and created file with tabNavigator routes

--- src/screens/auth/SuccessScreen/SuccessScreen.tsx
+++ src/screens/auth/SuccessScreen/SuccessScreen.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
+
 import {Button, Icon, Screen, Text} from '@components';
 import {AuthScreenProps} from '@routes';
-
 export function SuccessScreen({
   route,
   navigation,
