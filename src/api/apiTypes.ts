e9c5463cb6f1ff337cf1c366378db49200b9a2ad
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-09-10 17:02:59 -0300

    feat: created api folder with types file and modified app api layer files

---
+++ src/api/apiTypes.ts
@@ -0,0 +1,20 @@
+export interface MetaDataPageAPI {
+  current_page: number;
+  first_page: number;
+  first_page_url: string;
+  last_page: number;
+  last_page_url: string;
+  next_page_url: string | null;
+  per_page: number;
+  previous_page_url: string | null;
+  total: number;
+}
+
+/**
+ * @description interface que define o formato de uma página de dados da API.
+ * @template Data Tipo do dado da página.
+ */
+export interface PageAPI<Data> {
+  meta: MetaDataPageAPI;
+  data: Data[];
+}
--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,9 +1,16 @@
-import {postListMock} from './postListMock';
-import {Post} from './types';
+import {PageAPI} from '@api';
 
-async function getList(): Promise<Post[]> {
-  await new Promise(resolve => setInterval(() => resolve(''), 1000));
-  return postListMock;
+import {PostAPI} from './postTypes';
+
+async function getList(): Promise<PageAPI<PostAPI>> {
+  let response = await fetch('http://localhost:3333/user/post', {
+    headers: {
+      Authorization:
+        'Bearer Ng.K8712VlnJJX2JYQQrhSp4LxvgmFeCuOHqposBf0pfbtUoAV4Gk5IjgajeYNc',
+    },
+  });
+  let data: PageAPI<PostAPI> = await response.json();
+  return data;
 }
 
 export const postApi = {
