4861ef2492a5c331b34f663962ef26bf72d791e4
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-09-11 16:24:41 -0300

    feat: applied the design pattern adapter to the project and added the axios library

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,16 +1,10 @@
-import {PageAPI} from '@api';
+import {PageAPI, api} from '@api';
 
 import {PostAPI} from './postTypes';
 
 async function getList(): Promise<PageAPI<PostAPI>> {
-  let response = await fetch('http://localhost:3333/user/post', {
-    headers: {
-      Authorization:
-        'Bearer Ng.K8712VlnJJX2JYQQrhSp4LxvgmFeCuOHqposBf0pfbtUoAV4Gk5IjgajeYNc',
-    },
-  });
-  let data: PageAPI<PostAPI> = await response.json();
-  return data;
+  const response = await api.get<PageAPI<PostAPI>>('user/post');
+  return response.data;
 }
 
 export const postApi = {
