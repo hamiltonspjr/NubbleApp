4b900407f1245e59868c81a047b336dcd709fd85
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-09-25 15:27:56 -0300

    feat: added more information to screen

--- src/screens/app/PostCommentScreen/PostCommentScreen.tsx
+++ src/screens/app/PostCommentScreen/PostCommentScreen.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
+import {FlatList, ListRenderItemInfo} from 'react-native';
 
-// import {usePostCommentList} from 'src/domain/PostComment/useCases/usePostCommentList';
+import {PostComment, usePostCommentList} from '@domain';
 
-import {Box, Screen, Text} from '@components';
+import {Screen} from '@components';
+import {useAppSafeArea} from '@hooks';
 import {AppScreenProps} from '@routes';
 
+import {PostCommentBottom, PostCommentItem} from './components';
+
 export function PostCommentScreen({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   route,
 }: AppScreenProps<'PostCommentScreen'>) {
-  // const postId = route.params.postId;
-  //const {list} = usePostCommentList(postId);
+  const postId = route.params.postId;
+  const {list, fetchNextPage, hasNextPage} = usePostCommentList(postId);
+
+  const {bottom} = useAppSafeArea();
+
+  function renderItem({item}: ListRenderItemInfo<PostComment>) {
+    return <PostCommentItem postComment={item} />;
+  }
+
   return (
     <Screen title="Comentários" canGoBack>
-      <Box>
-        <Text>Tela de comentários</Text>
-      </Box>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={list}
+        renderItem={renderItem}
+        contentContainerStyle={{paddingBottom: bottom}}
+        ListFooterComponent={
+          <PostCommentBottom
+            hasNextPage={hasNextPage}
+            fetchNextPage={fetchNextPage}
+          />
+        }
+      />
     </Screen>
   );
 }
