2394cbca59936008df11249434458a33b9b394e9
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-10-03 09:52:36 -0300

    feat: modified the login token used

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer Mw.tDA4OXH1LOtlsBQn5g5ir0rAgEArwdZNEhRDsY1cEwbxfS985piwNq5TbOyy',
+      'Bearer NQ.CsTPNQms6KrPuR3hiocsu4gkNSnl9md92qREHrPrG5v3jm3lEVF3mUcbx7q4',
   },
 });
