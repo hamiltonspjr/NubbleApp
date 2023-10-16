f9184341a56d06258a4a913f80134f6125ffc4f0
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-10-16 17:19:11 -0300

    feat(storage): added user login storage functionality

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,11 +3,24 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {useAuthCredentials} from '@services';
 
+import {ActivityIndicator, Box} from '@components';
+
 import {AppStack} from './AppStack';
 import {AuthStack} from './AuthStack';
 
 export function Router() {
-  const {authCredentials} = useAuthCredentials();
+  const {authCredentials, isLoading} = useAuthCredentials();
+  if (isLoading) {
+    return (
+      <Box
+        flex={1}
+        backgroundColor="background"
+        justifyContent="center"
+        alignItems="center">
+        <ActivityIndicator />
+      </Box>
+    );
+  }
   return (
     <NavigationContainer>
       {authCredentials ? <AppStack /> : <AuthStack />}
--- src/services/authCredentials/useAuthCredentials.ts
+++ src/services/authCredentials/useAuthCredentials.ts
@@ -1,14 +1,31 @@
-import {create} from 'zustand';
+import {useContext} from 'react';
 
 import {AuthCredentialsService} from './authCredentialsType';
+import {AuthCredentialsContext} from './Providers/AuthCredentialsProvider';
 
 export function useAuthCredentials(): AuthCredentialsService {
-  return useAuthCredentialsZustand();
+  const context = useContext(AuthCredentialsContext);
+
+  if (!context) {
+    throw new Error(
+      'useAuthCredentials must be used within a AuthCredentialsProvider',
+    );
+  }
+
+  return context;
 }
 
-const useAuthCredentialsZustand = create<AuthCredentialsService>(set => ({
-  authCredentials: null,
-  isLoading: false,
-  saveCredentials: async ac => set({authCredentials: ac}),
-  removeCredentials: async () => set({authCredentials: null}),
-}));
+// const useAuthCredentialsZustand = create<AuthCredentialsService>()(
+//   persist(
+//     set => ({
+//       authCredentials: null,
+//       isLoading: false,
+//       saveCredentials: async ac => set({authCredentials: ac}),
+//       removeCredentials: async () => set({authCredentials: null}),
+//     }),
+//     {
+//       name: '@Auth',
+//       storage: storage,
+//     },
+//   ),
+// );
